aac88fbac862d6bee72d6a3515453d8441fe1957
Yaron Benjamin <ybenjami@gmail.com>
2020-08-15 10:07:25 -0400

    First Commit with Bubble Sort

---
+++ README.md
@@ -0,0 +1,2 @@
+# Back to the Basics: Sample Algorithms
+This repository for the 'Back to the Basics series'
---
+++ src/index.js
@@ -0,0 +1,25 @@
+
+
+const bubbleSort = async (intArray) => {
+    for(let lastUnsortedIndex = intArray.length -1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+        for(let i = 0; i < lastUnsortedIndex; i++){
+            if(intArray[i] > intArray[i +1]) {
+                swap(intArray, i, i+1);
+            }
+        }
+
+    }
+    return intArray;
+}
+ const swap = (array,  i,  j) =>{
+    if(i== j){
+        return;
+    }
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+   
+const intArray = [80, 95, -90, 81, 39, 69];
+bubbleSort(intArray);
+console.log(intArray);
